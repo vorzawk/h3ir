085ac84e065be564cb55c2dd779577993308d439
abhilash <21965736+vorzawk@users.noreply.github.com>
2018-03-31 17:26:17 -0500

    regex used because I could not figure out how to use translate in python 2

--- p1_embeddingBrownCorpus.py
+++ p1_embeddingBrownCorpus.py
@@ -8,8 +8,8 @@ listWords = brown.words()
 brown_noStopWords = [w for w in listWords if w.lower() not in stop_words]
 
 # Remove punctuation
-table = str.maketrans({key: None for key in string.punctuation})
-brown_noPunctuation = [w.translate(table) for w in brown_noStopWords]
+import re
+brown_noPunctuation = [re.sub(r'[^\w]','',w) for w in brown_noStopWords]
 # Remove the leftover empty strings, so that they don't get counted in the
 # window operation
 brown_noPunctuation = [w for w in brown_noPunctuation if w != ''] 
